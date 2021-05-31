4c8dc446c56530d60687d5e63f4722a24d241475
chinsn810 <chinmay.suresh@gmail.com>
2021-05-31 23:11:28 +0530

    Add files via upload

---
+++ ProjCov19Vacc.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[31]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import requests
+import urllib.request
+import json
+from io import StringIO
+import plotly.express as px
+import plotly.io as pio
+import scipy.stats as stats
+import statsmodels.api as sm
+import pickle
+from PIL import Image
+from sklearn import metrics
+pio.renderers.default = 'browser'
+
+
+# # Data 
+
+# In[32]:
+
+
+s_code = pd.read_csv(r'C:/CHINMAY/Documents/s_code.csv')
+gsheetid_1 = "1noqoXm0pnb61miW0HnaCCNsKPm_4lwhbzmHkREuF0ZY"
+sheet_name_1 = "Vaccine"
+gsheet_url_1 = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid_1,sheet_name_1)
+vacc = pd.read_csv(gsheet_url_1)
+vacc = vacc[vacc["State"]!="India"]
+
+
+# In[33]:
+
+
+vacc = vacc.groupby('State').last().reset_index()
+vacc = vacc.drop(columns=['Updated On'])
+vacc = vacc.drop(index=[33],axis=0)
+vacc.replace(',','',regex=True,inplace=True)
+
+
+# In[34]:
+
+
+vacc["id"] = s_code["id"]
+
+
+# In[35]:
+
+
+states = json.load(open("states_india.geojson", "r"))
+state_id_map = {}
+for feature in states["features"]:
+    feature["id"] = feature["properties"]["state_code"]
+    state_id_map[feature["properties"]["st_nm"]] = feature["id"]
+
+
+# # Model
+
+# In[36]:
+
+
+c=vacc.corr()
+top_corr_features=c.index
+plt.figure(figsize=(10,5))
+g=sns.heatmap(vacc[top_corr_features].corr(),annot=True,cmap='RdYlGn')
+
+
+# In[40]:
+
+
+new_data= vacc.drop(['State','First Dose Administered', 'Second Dose Administered',
+       'Male(Individuals Vaccinated)', 'Female(Individuals Vaccinated)',
+       'Transgender(Individuals Vaccinated)', 'Total Covaxin Administered',
+       'Total CoviShield Administered', 'AEFI', '18-45 years (Age)', '45-60 years (Age)', '60+ years (Age)',
+       'Total Doses Administered','Total Sputnik V Administered',
+       'Total Doses Administered','id'],axis=1)
+
+
+# In[41]:
+
+
+X = new_data.drop(['Total Individuals Vaccinated'],axis=1)
+y = new_data['Total Individuals Vaccinated']
+
+
+# In[42]:
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size =0.30, random_state=2)
+
+
+# In[43]:
+
+
+from sklearn.linear_model import LinearRegression
+LR = LinearRegression()
+model = LR.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+
+
+# In[44]:
+
+
+model.score(X_train,y_train)
+
+
+# In[45]:
+
+
+model.score(X_test,y_test)
+
+
+# In[47]:
+
+
+x1 = sm.add_constant(X_train)
+model1 = sm.OLS(y_train,x1)
+results = model1.fit()
+print(results.summary())
+
+
+# In[16]:
+
+
+#Normality of Residual
+residuals = y_test - y_pred
+sns.distplot((residuals))
+
+
+# In[17]:
+
+
+plt.plot(X_test['Total Sites'],y_pred,label = 'Linear Regression', color = 'b')
+plt.scatter(X_test['Total Sites'],y_test,label = 'Actual test data',
+            color = 'g',alpha=.7)
+plt.legend()
+plt.show()
+
+
+# In[18]:
+
+
+residuals = y_test - y_pred
+fig, ax = plt.subplots(figsize=(6,5))
+_ = ax.scatter(y_pred, residuals)
+
+
+# In[19]:
+
+
+pickle.dump(model,open('vaccine.pkl','wb'))
+
+
+# # Streamlit 
+
+# In[20]:
+
+
+st.markdown("# COVID-19 Vaccination Detailed Analysis (India)") 
+img = Image.open('C:/CHINMAY/Documents/COVID-19-vaccine.png')
+if img.mode != 'RGB':
+    img = img.convert('RGB')
+st.image(img, caption = "Source: bioworld.com", width=500)
+if st.checkbox('view_data'):
+    st.subheader('Vaccination Data')
+    st.write(vacc)
+
+
+# In[21]:
+
+
+st.sidebar.markdown("## Side Panel")
+st.sidebar.markdown("Use this panel to explore our app")
+
+st.sidebar.subheader('Visualizations')
+if st.sidebar.checkbox('Individuals Vaccinated'):
+    st.subheader('Number of Individuals Vaccinated by State')
+    fig = px.bar(vacc, x="State", y="Total Individuals Vaccinated", height=800, width = 800)
+    fig.update_layout(xaxis={'categoryorder':'total descending'})
+    st.plotly_chart(fig)
+
+if st.sidebar.checkbox('Covid-19 Vaccines in India'):
+    st.subheader('Vaccines Administered by State')
+    fig = px.bar(vacc, x="State", y=["Total Covaxin Administered","Total CoviShield Administered"], 
+             barmode='group', height=800, width = 800)
+    st.plotly_chart(fig)
+    
+if st.sidebar.checkbox('Gender'):
+    st.subheader('Vaccines Administered by State and Gender')
+    fig = px.bar(vacc, x="State", y=["Male(Individuals Vaccinated)","Female(Individuals Vaccinated)",
+                                     "Transgender(Individuals Vaccinated)"], 
+             barmode='group', height=800, width = 800)
+    st.plotly_chart(fig)
+    
+if st.sidebar.checkbox('First Dose and Second Dose'):
+    st.subheader('First and Second Doses Administered by State')
+    fig = px.bar(vacc, x="State", y=['First Dose Administered','Second Dose Administered'], 
+             barmode='group', height=800, width = 800)
+    fig.update_layout(xaxis={'categoryorder':'total descending'})
+    st.plotly_chart(fig)
+    
+if st.sidebar.checkbox('Map'):
+    st.subheader('Covid19 Vaccine Map')
+    fig = px.choropleth_mapbox(
+    vacc,
+    locations="id",
+    geojson = states,
+    color="Total Individuals Vaccinated",
+    hover_name="State",
+    hover_data=['Total Individuals Vaccinated', 'Total Sessions Conducted',
+       'Total Sites', 'First Dose Administered', 'Second Dose Administered',
+       'Male(Individuals Vaccinated)', 'Female(Individuals Vaccinated)',
+       'Transgender(Individuals Vaccinated)', 'Total Covaxin Administered',
+       'Total CoviShield Administered'],
+    title="India Covid-19 Vaccine Map",
+    mapbox_style="carto-positron",
+    center={"lat": 24, "lon": 78},
+    zoom=3,
+    opacity=0.5,
+    )
+    st.plotly_chart(fig)
+
+
+# In[22]:
+
+
+model=pickle.load(open('vaccine.pkl','rb'))
+st.sidebar.header("Model")
+if st.sidebar.checkbox("Predict"):
+    def predict(sites_no,sessions_no):
+        input=np.array([[sites_no,sessions_no]]).astype(np.float64)
+        prediction=model.predict(input)
+        return float(prediction)
+
+    def main():
+        st.title("Predictor")
+        html_temp = """
+        <div style="background-color:blue ;padding:10px">
+        <h2 style="color:white;text-align:center;">Covid19 ML App </h2>
+        </div>
+        """
+        st.markdown(html_temp, unsafe_allow_html=True)
+        
+        sites_no = st.text_input("Number of Sessions","Type Here")
+        sessions_no = st.text_input("Number of Sites","Type Here")
+
+        if st.button("Predict"):
+            output = predict(sites_no,sessions_no)
+            st.success('Total Number of Individuals Vaccinated is {}'.format(output))
+
+        fig = px.choropleth_mapbox(vacc,locations="id",geojson = states,
+                                   color="Total Individuals Vaccinated",hover_name="State",
+                                   hover_data = ['Total Individuals Vaccinated', 'Total Sessions Conducted',
+                                                   'Total Sites'],
+                                   mapbox_style="carto-positron",center={"lat": 24, "lon": 78},
+                                   zoom=3,opacity=0.5,
+                                   )
+        st.plotly_chart(fig)
+
+    if __name__=='__main__':
+        main()
+
